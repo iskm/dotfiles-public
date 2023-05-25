7c93b20ae4fb14d2b5e0353e293fa9bb0a4fd45f
Ibrahim Mkusa <ibrahimmkusa@gmail.com>
2023-05-24 21:06:05 -0400

    v0.99.0

--- README.md
+++ README.md
@@ -8,4 +8,10 @@ Install via you favorite package manager
 
 Use `stow` to symlink these dotfiles to your `~`
 
+You can configure manually or run it from root of this cloned directory
+The script file assumes you are running a deb variant
+`git clone git@github.com:iskm/dotfiles-public.git ~`
+`cd dotfiles-public`
+`./install.sh`
+
 #### Buena suerte, Amigos
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Author: Ibahim Mkusa
+# About: setup dotfiles
+
+set -e
+cd ~
+sudo apt update -yy && sudo apt upgrade -yy
+
+# install build essential libs
+sudo apt install -y build-essential cmake python3-dev
+
+# install core programs
+sudo apt install -y vim-nox git tmux neofetch curl ranger silversearcher-ag \
+  direnv glances gedit-plugins zsh
+
+# prompt to install tmux plugin manager
+echo -n "Install tmuxifier[yes/no]?"
+read -r response
+if [[ "$response" == "yes" ]]; then
+  git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier || true
+fi
+
+# Install tpm
+echo -n "Install tmux plugin manager[yes/no]?"
+read -r response
+if [[ "$response" == "yes" ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || true 
+fi
+
+# Change Shell to zsh
+echo -n "Change shell to zsh[yes/no]?"
+read -r response
+if [[ "$response" == "yes" ]]; then
+  chsh -s /usr/bin/zsh
+fi
+
+# link files using stow
+rm -rf ~/.bashrc ~/.bash_profile ~/.zshrc
+cd ~/dotfiles-public
+stow bash git ranger shellenv tmux vim zsh
+cd -
+
+echo "Good luck, Amigo!!"
+
+
